18b30b38c3754b85afb0123b66f943343a8538c8
Domisnnet <dominique.goncalves61@gmail.com>
2026-06-26 09:05:58 -0300

    refatorados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,19 +9,19 @@ export const routes: Routes = [
   {
     path: 'home',
     loadComponent: () =>
-      import('./pages/home/home')
+      import('./features/home/home')
       .then(c => c.Home)
   },
   {
     path: 'quiz',
     loadComponent: () =>
-      import('./pages/quiz/quiz')
+      import('./features/quiz/quiz')
       .then(c => c.Quiz)
   },
   {
     path: 'result',
     loadComponent: () =>
-      import('./pages/result/result')
+      import('./features/result/result')
       .then(c => c.Result)
   },
   {
